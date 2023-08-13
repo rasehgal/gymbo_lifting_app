56433b2ab8aa62d75073fd889078fc62061940c8
rasehgal <rsehgal95@gmail.com>
2023-08-12 17:55:36 -0700

    Initial Commit

---
+++ Gymbo_Math/Gymbo_MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gymbo_MathApp.swift
+//  Gymbo_Math
+//
+//  Created by Rashmi S on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Gymbo_MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
